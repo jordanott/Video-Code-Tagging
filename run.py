813b362c9a08f9bbbc8365852264add3dc38da7e
jordanott <jordanott365@gmail.com>
2017-09-14 09:42:16 -0700

    Load data

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
-# train model
+import sys
+sys.path.append('Model/')
+from model import cnn
+
+def to_seconds(t):
+    count = 0
+    total = 0
+    for i in reversed(t.split(':')):
+        total += 60**count * int(i)
+        count += 1
+    return total
+
+def get_data():
+    DATA_PATH = 'DataCollection/'
+    links = open(DATA_PATH+'links.txt')
+    data = {}
+    for line in links:
+        link,name,code_time,nocode_time = line.split(' | ')
+        link,name,code_time,nocode_time = link.strip(),name.strip(),code_time.strip(),nocode_time.strip()
+        data[name] = [code_time.split(','),nocode_time.split(',')]
+    return data
+
+data = get_data()
+print data
