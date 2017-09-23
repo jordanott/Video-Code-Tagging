67c7074116fd46a9ba605678e13c81e6a4a3bab8
jordanott <jordanott365@gmail.com>
2017-09-23 10:10:35 -0700

    Dir names with spaces

--- DataCollection/grab.sh
+++ DataCollection/grab.sh
@@ -7,32 +7,32 @@ do
   while IFS=' | ' read -ra line
   do
     url=${line[0]}
-    name=${line[1]}
+    name=${line[@]:1}
   done <<< "$var"
   # download video
   {
     path=Downloads/$name"_720"
-    mkdir $path
-    pytube -e mp4 -p $path/ -f $name -r 720p $url
+    mkdir "$path"
+    pytube -e mp4 -p "$path"/ -f "$name" -r 720p $url
   }||
   {
-    rm -rf $path
+    rm -rf "$path"
     path=Downloads/$name"_480"
     mkdir $path
-    pytube -e mp4 -p $path/ -f $name -r 480p $url
+    pytube -e mp4 -p "$path"/ -f "$name" -r 480p $url
   }||
   {
-    rm -rf $path
+    rm -rf "$path"
     path=Downloads/$name"_360"
-    mkdir $path
-    pytube -e mp4 -p Downloads/$name"_360"/ -f $name -r 360p $url
+    mkdir "$path"
+    pytube -e mp4 -p "$path"/ -f "$name" -r 360p $url
   }||
   {
-    rm -rf $path
+    rm -rf "$path"
     echo $url >> error.txt
   }
   # location of mp4 file
-  video=$path/$name.mp4
+  video="$path/$name.mp4"
   # split videos 1 frame per second
-  ffmpeg -i $video -r 1 -f image2 $path/%d.png -nostdin
+  ffmpeg -i "$video" -r 1 -f image2 "$path"/%d.png -nostdin
 done < "$input"
