aa7457ece9e08f62f3e6ba461b90921c2b184d51
Jordan Ott <jordanott365@gmail.com>
2017-10-03 10:40:46 -0700

    Inception

--- Models/CNN/model.py
+++ Models/CNN/model.py
@@ -1,3 +1,51 @@
-# implementation of convolutional network
-def cnn():
-    pass
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D, Input
+from keras import backend as K
+from keras.optimizers import SGD
+import numpy as np
+
+def Inception(input_shape,num_classes):
+    input_tensor = Input(shape=input_shape)
+    base_model = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    # output layer with 'num_classes' number of catergories
+    predictions = Dense(num_classes, activation='softmax')(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+    # train the model on the new data for a few epochs
+    #model.fit_generator(...)
+
+    # at this point, the top layers are well trained and we can start fine-tuning
+    # convolutional layers from inception V3. We will freeze the bottom N layers
+    # and train the remaining top layers.
+
+    # let's visualize layer names and layer indices to see how many layers
+    # we should freeze:
+    for i, layer in enumerate(base_model.layers):
+       print(i, layer.name)
+    '''
+    # we chose to train the top 2 inception blocks, i.e. we will freeze
+    # the first 249 layers and unfreeze the rest:
+    for layer in model.layers[:249]:
+       layer.trainable = False
+    for layer in model.layers[249:]:
+       layer.trainable = True
+
+    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+    '''
+    return model
