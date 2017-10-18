5084ed00e1095944b28fe8e60535d2908f3e8ce3
Jordan Ott <jordanott365@gmail.com>
2017-10-17 22:59:30 -0700

    Data augmentation set up

--- run.py
+++ run.py
@@ -1,24 +1,27 @@
 import sys
+from load_data import load_data
 sys.path.append('Model/')
-from model import cnn
+from model import Inception
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
-def to_seconds(t):
-    count = 0
-    total = 0
-    for i in reversed(t.split(':')):
-        total += 60**count * int(i)
-        count += 1
-    return total
+batch_size = 32
+epochs = 15
 
-def get_data():
-    DATA_PATH = 'DataCollection/'
-    links = open(DATA_PATH+'links.txt')
-    data = {}
-    for line in links:
-        link,name,code_time,nocode_time = line.split(' | ')
-        link,name,code_time,nocode_time = link.strip(),name.strip(),code_time.strip(),nocode_time.strip()
-        data[name] = [code_time.split(','),nocode_time.split(',')]
-    return data
+x_train,y_train,x_test,y_test = load_data()
+model = Inception((250,250,3),2)
 
-data = get_data()
-print data
+
+datagen = ImageDataGenerator(
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.2,
+        fill_mode='nearest')
+        
+datagen.fit(x_train)
+
+model.fit_generator(datagen.flow(x_train, y_train,
+    	    batch_size=batch_size),
+    	    steps_per_epoch=x_train.shape[0] // batch_size,
+    	    epochs=epochs,
+    	    validation_data=(x_test, y_test))
