2538359a7097a285f18d625b6a16d418b4d86078
Jordan Ott <jordanott365@gmail.com>
2017-11-01 14:15:41 -0700

    Leave one out train

--- run.py
+++ run.py
@@ -1,54 +1,99 @@
-import sys
-from load_data import load_data
-sys.path.append('Models/CNN/')
-from model import Inception
+from load_data import load_data,load_data_leave_one_out
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint,EarlyStopping,TensorBoard
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
+import sys
+sys.path.append('Models/CNN/')
+from model import Inception
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--load',action="store_true")
 parser.add_argument('--show',action="store_true")
 parser.add_argument('--train',action="store_true")
+parser.add_argument('--leave_one_out',action="store_true")
 args = parser.parse_args()
 # visualize the images and choices
 LOAD = args.load
 TRAIN = args.train
 VISUALIZE = args.show
+LEAVE_ONE_OUT = args.leave_one_out
 
 batch_size = 32
 epochs = 5
 PATIENCE = 2
 
-# load data
-x_train,y_train,x_test,y_test = load_data()
-print 'Code samples:',np.sum(y_train[:,0])
-print 'No C samples:',np.sum(y_train[:,1])
-model = Inception((300,300,3),2)
-
 
 if TRAIN:
-	datagen = ImageDataGenerator(
-		width_shift_range=0.1,
-		height_shift_range=0.1,
-		zoom_range=0.2,
-		fill_mode='nearest')
-	datagen.fit(x_train)
+	if LEAVE_ONE_OUT:
+		os.mkdir('Weights')
+		os.mkdir('TensorBoard')
+		log = open('log.txt','w')
+		videos = next(os.walk('../Data/'))[1]
+		count = 0
+		for directory in videos:
+			count += 1
+			x_train,y_train,x_test,y_test = load_data_leave_one_out(directory)
+			log.write(str(count)+','+directory+',Code:'+str(np.sum(y_train[:,0]))+',No C:'+str(np.sum(y_train[:,1]))+
+				',Train:'+str(len(x_train))+',Test:'+str(len(x_test)))
+			# load pretrained network
+			model = Inception((300,300,3),2)
+			# data augmentation
+			datagen = ImageDataGenerator(
+				width_shift_range=0.1,
+				height_shift_range=0.1,
+				zoom_range=0.2,
+				fill_mode='nearest')
+			datagen.fit(x_train)
+			# Callbacks
+			tb = TensorBoard(log_dir='TensorBoard/{}'.format(count),histogram_freq=0, write_graph=True, write_images=True)
+			es = EarlyStopping(monitor='val_acc', min_delta=0, patience=PATIENCE, verbose=0, mode='auto')
+			w = ModelCheckpoint("Weights/{}.h5".format(count),monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+			# Training
+			history = model.fit_generator(datagen.flow(x_train, y_train,
+				batch_size=batch_size),
+				steps_per_epoch=x_train.shape[0] // batch_size,
+				epochs=1,
+				validation_data=(x_test, y_test),
+				callbacks=[tb,es,w])
 
-	# Callbacks
-	tb = TensorBoard(log_dir='TensorBoard',histogram_freq=0, write_graph=True, write_images=True)
-	es = EarlyStopping(monitor='val_acc', min_delta=0, patience=PATIENCE, verbose=0, mode='auto')
-	w = ModelCheckpoint("code_tagger_weights.h5",monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-	# Training
-	model.fit_generator(datagen.flow(x_train, y_train,
-		batch_size=batch_size),
-		steps_per_epoch=x_train.shape[0] // batch_size,
-		epochs=epochs,
-		validation_data=(x_test, y_test),
-		callbacks=[tb,es,w])
+			log.write(',Max val acc:'+str(max(history.history['val_acc']))+'\n')
+
+
+	else:
+		# load data
+		x_train,y_train,x_test,y_test = load_data()
+		print 'Code samples:',np.sum(y_train[:,0]),'No C samples:',np.sum(y_train[:,1])
+		print 'Train:',len(x_train),'Test:',len(x_test)
+		model = Inception((300,300,3),2)
+
+		datagen = ImageDataGenerator(
+			width_shift_range=0.1,
+			height_shift_range=0.1,
+			zoom_range=0.2,
+			fill_mode='nearest')
+		datagen.fit(x_train)
+
+		# Callbacks
+		tb = TensorBoard(log_dir='TensorBoard',histogram_freq=0, write_graph=True, write_images=True)
+		es = EarlyStopping(monitor='val_acc', min_delta=0, patience=PATIENCE, verbose=0, mode='auto')
+		w = ModelCheckpoint("code_tagger_weights.h5",monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+		# Training
+		model.fit_generator(datagen.flow(x_train, y_train,
+			batch_size=batch_size),
+			steps_per_epoch=x_train.shape[0] // batch_size,
+			epochs=epochs,
+			validation_data=(x_test, y_test),
+			callbacks=[tb,es,w])
 elif LOAD:
+	# load data
+	x_train,y_train,x_test,y_test = load_data()
+	print 'Code samples:',np.sum(y_train[:,0])
+	print 'No C samples:',np.sum(y_train[:,1])
+	model = Inception((300,300,3),2)
+
 	model.load_weights('code_tagger_weights.h5')
 	print 'Code samples:',np.sum(y_test[:,0])
 	print 'No C samples:',np.sum(y_test[:,1])
@@ -58,10 +103,12 @@ elif LOAD:
 		actual = np.argmax(y_test[i])
 		if prediction == actual:
 			correct += 1
+		if (i+1) % 300 == 0:
+			print correct/float(i)
 		if VISUALIZE:
 			print prediction
 			print actual
 			plt.imshow(x_test[i])
 			plt.show()
 
-	print 'Accuracy:',correct/len(x_test)
+	print 'Accuracy:',correct/float(len(x_test))
