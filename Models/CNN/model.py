f6522f93c067a33487c3dc4a22c34139e86d60b5
Jordan Ott <jordanott365@gmail.com>
2017-11-08 12:23:56 -0800

    VGG16

--- Models/CNN/model.py
+++ Models/CNN/model.py
@@ -1,4 +1,5 @@
 from keras.applications.inception_v3 import InceptionV3
+from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D, Input
@@ -27,23 +28,56 @@ def Inception(input_shape,num_classes):
     # compile the model (should be done *after* setting layers to non-trainable)
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
 
-    # train the model on the new data for a few epochs
-    #model.fit_generator(...)
-
-    # at this point, the top layers are well trained and we can start fine-tuning
-    # convolutional layers from inception V3. We will freeze the bottom N layers
-    # and train the remaining top layers.
-
-    # let's visualize layer names and layer indices to see how many layers
-    # we should freeze:
-    '''
-    # we chose to train the top 2 inception blocks, i.e. we will freeze
-    # the first 249 layers and unfreeze the rest:
-    for layer in model.layers[:249]:
-       layer.trainable = False
-    for layer in model.layers[249:]:
-       layer.trainable = True
-
-    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
-    '''
     return model
+
+def VGG(input_shape,num_classes):
+    input_tensor = Input(shape=input_shape)
+    base_model = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    #x = Dense(1024, activation='relu')(x)
+    # output layer with 'num_classes' number of catergories
+    predictions = Dense(num_classes, activation='softmax')(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
+    return model
+
+def get_output_layer(model, layer_name):
+    # get the symbolic outputs of each "key" layer (we gave them unique names).
+    layer_dict = dict([(layer.name, layer) for layer in model.layers])
+    layer = layer_dict[layer_name]
+    return layer
+
+def visualize_class_activation_map(model, img, output_path):
+    # TODO: fix and test
+    import cv2
+    width, height, _ = original_img.shape
+
+    #Get the 512 input weights to the softmax.
+    class_weights = model.layers[-1].get_weights()[0]
+    final_conv_layer = get_output_layer(model, "block5_conv3")
+    get_output = K.function([model.layers[0].input], [final_conv_layer.output, model.layers[-1].output])
+    [conv_outputs, predictions] = get_output([img])
+    conv_outputs = conv_outputs[0, :, :, :]
+
+    #Create the class activation map.
+    cam = np.zeros(dtype = np.float32, shape = conv_outputs.shape[1:3])
+    for i, w in enumerate(class_weights[:, 1]):
+            cam += w * conv_outputs[i, :, :]
+    print "predictions", predictions
+    cam /= np.max(cam)
+    cam = cv2.resize(cam, (height, width))
+    heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
+    heatmap[np.where(cam < 0.2)] = 0
+    img = heatmap*0.5 + original_img
+    cv2.imwrite(output_path, img)
