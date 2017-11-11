52f9db3b958e90458f7ed07142e8b28c752901a2
Jordan Ott <jordanott365@gmail.com>
2017-11-10 18:07:48 -0800

    Save data to file

--- load_data.py
+++ load_data.py
@@ -57,59 +57,64 @@ def load_data_leave_one_out(directory):
     return np.array(x_train),np.array(y_train),np.array(x_test),np.array(y_test)
 
 
-def load_data(split=.8,seed=0,single=False,prefix=''):
-    DATA_DIR = prefix+'../Data'
-    labels_json = json.load(open(prefix+'datacleaned.json'))
-    images = []
-    labels = []
-    for i in range(len(labels_json)):
-        file_location = str(labels_json[i]['fileName'])
-        entry_labels = [labels_json[i]['isCodeCount'],labels_json[i]['isPartiallyCodeCount'],
-            labels_json[i]['isHandWrittenCount'],labels_json[i]['isNotCodeCount']]
-        index = np.argmax(np.array(entry_labels))
-        max_value = np.amax(np.array(entry_labels))
-        LOAD = False
-        if max_value > 0:
-            # the image contains code
-            if index == 0:
-                labels.append(np.array([1,0]))
-                LOAD = True
-            # the image has partially visible code
-            elif index == 1:
-                pass
-            # the image has hand written code
-            elif index == 2:
-                pass
-            # the image does not contain code
-            elif index == 3:
-                labels.append(np.array([0,1]))
-                LOAD = True
-            else:
-                pass
-        if LOAD:
-            complete_location = DATA_DIR+file_location.replace('.png','_resized.png')
-            img = Image.open(complete_location)
-            if img.mode != 'RGB':
-               img = img.convert('RGB')
-            img = np.asarray(img)
-            images.append(img)
-            if single and len(images) > 1000:
-                return np.array(images),np.array(labels)
-    random.seed(seed)
-    images = np.array(images)
-    images,labels = shuffle(images,labels)
-    split_index = int(len(images)*split)
-    x_train,x_test = images[:split_index],images[split_index:]
-    y_train,y_test = labels[:split_index],labels[split_index:]
+def load_data(split=.8,seed=0,single=False,prefix='',load_new=False):
+    if load_new:
+        DATA_DIR = prefix+'../Data'
+        labels_json = json.load(open(prefix+'datacleaned.json'))
+        images = []
+        labels = []
+        for i in range(len(labels_json)):
+            file_location = str(labels_json[i]['fileName'])
+            entry_labels = [labels_json[i]['isCodeCount'],labels_json[i]['isPartiallyCodeCount'],
+                labels_json[i]['isHandWrittenCount'],labels_json[i]['isNotCodeCount']]
+            index = np.argmax(np.array(entry_labels))
+            max_value = np.amax(np.array(entry_labels))
+            LOAD = False
+            if max_value > 0:
+                # the image contains code
+                if index == 0:
+                    labels.append(np.array([1,0,0,0]))
+                    LOAD = True
+                # the image has partially visible code
+                elif index == 1:
+                    labels.append(np.array([0,1,0,0]))
+                    LOAD = True
+                # the image has hand written code
+                elif index == 2:
+                    labels.append(np.array([0,0,1,0]))
+                    LOAD = True
+                # the image does not contain code
+                elif index == 3:
+                    labels.append(np.array([0,0,0,1]))
+                    LOAD = True
+            if LOAD:
+                complete_location = DATA_DIR+file_location.replace('.png','_resized.png')
+                img = Image.open(complete_location)
+                if img.mode != 'RGB':
+                   img = img.convert('RGB')
+                img = np.asarray(img)
+                images.append(img)
+                if single and len(images) > 12000:
+                    return np.array(images),np.array(labels)
+        random.seed(seed)
+        images = np.array(images)
+        images,labels = shuffle(images,labels)
+        split_index = int(len(images)*split)
+        x_train,x_test = images[:split_index],images[split_index:]
+        y_train,y_test = labels[:split_index],labels[split_index:]
 
-    count = 0
-    for i in range(len(x_train)):
-        for j in range(len(x_test)):
-            dist = np.linalg.norm(x_train[i]-x_test[j])
-            if dist == 0:
-                np.delete(x_test,j,0)
-                np.delete(y_test,j,0)
-                count += 1
-    print 'Deleted:',count
+        count = 0
+        for i in range(len(x_train)):
+            for j in range(len(x_test)):
+                dist = np.linalg.norm(x_train[i]-x_test[j])
+                if dist == 0:
+                    np.delete(x_test,j,0)
+                    np.delete(y_test,j,0)
+                    count += 1
+        print 'Deleted:',count
+        np.savez('data',x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test)
+    else:
+        data = np.load('data.npz')
+        x_train,y_train,x_test,y_test = data['x_train'],data['y_train'],data['x_test'],data['y_test']
 
     return x_train,y_train,x_test,y_test
