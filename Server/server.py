0ad6efc7f37d22e360443356273e71872121ccdf
Jordan Ott <jordanott365@gmail.com>
2017-12-08 12:17:21 -0800

    Server updates

--- Server/server.py
+++ Server/server.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/link/<int:link>')
+def get_link(link):
+    print link
+    # show the post with the given id, the id is an integer
+    return 'Link %d' % link
+
 if __name__ == "__main__":
     # connect to ip adress
     app.run(host='0.0.0.0')
