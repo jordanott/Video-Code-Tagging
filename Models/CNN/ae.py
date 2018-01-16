fc2bb56f8813dffcbaa59ae50f4b41cdc740fb9f
Jordan Ott <jordanott365@gmail.com>
2018-01-16 08:47:52 -0800

    Predictions

--- Models/CNN/ae.py
+++ Models/CNN/ae.py
@@ -2,6 +2,7 @@ from keras.callbacks import TensorBoard
 import numpy as np
 from model import conv_ae
 
+TRAIN = False
 batch_size = 32
 epochs = 500
 PATIENCE = 20
@@ -12,9 +13,19 @@ x_train,y_train,x_test,y_test = data['x_train'],data['y_train'],data['x_test'],d
 print 'Data loaded...'
 model = conv_ae((300,300,3))
 print 'Model loaded...'
-model.fit(x_train, x_train,
+if TRAIN:
+    model.fit(x_train, x_train,
                 epochs=epochs,
                 batch_size=batch_size,
                 shuffle=True,
                 validation_data=(x_test, x_test),
                 callbacks=[TensorBoard(log_dir='autoencoder')])
+    model.save_weights('ae.h5')
+else:
+    model.load_weights('ae.h5')
+    for _ in range(7):
+        model.layers.pop()
+    print model.summary()
+
+    encodings = model.predict(x_test)
+    print encodings.shape
