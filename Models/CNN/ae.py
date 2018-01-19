760f1ad61c1e52a47e942b89ef5c81babc63b4ab
Jordan Ott <jordanott365@gmail.com>
2018-01-18 16:50:00 -0800

    Train on more images

--- Models/CNN/ae.py
+++ Models/CNN/ae.py
@@ -1,24 +1,30 @@
+from keras.preprocessing.image import load_img
 from keras.callbacks import TensorBoard
 import numpy as np
 from model import conv_ae,conv_e
+import os
 
-TRAIN = False
+TRAIN = True
 batch_size = 32
 epochs = 500
 PATIENCE = 20
 
-
-data = np.load('../../Fold_0/data.npz')
-x_train,y_train,x_test,y_test = data['x_train'],data['y_train'],data['x_test'],data['y_test']
-print 'Data loaded...'
+images = np.empty((1,300,300,3))
+for subdir,dirs,files in os.walk('../../../Data/'):
+    for img in files:
+        if img.endswith('_resized.png'):
+            img_path = os.path.join(subdir,img)
+            image = np.array(load_img(img_path,target_size=(300,300,3))).reshape(1,300,300,3)
+            images = np.append(images,image,axis=0)
+print 'Data loaded...', images.shape
 model = conv_ae((300,300,3))
 print 'Model loaded...'
 if TRAIN:
-    model.fit(x_train, x_train,
+    model.fit(images, images,
                 epochs=epochs,
                 batch_size=batch_size,
                 shuffle=True,
-                validation_data=(x_test, x_test),
+                validation_data=(images, images),
                 callbacks=[TensorBoard(log_dir='autoencoder')])
     model.save_weights('ae.h5')
 else:
@@ -37,15 +43,15 @@ else:
         ax[0].imshow(img1)
         ax[1].imshow(img2)
         plt.show()
-    
+
     def dist(vector):
         return np.linalg.norm(encodings[0] - vector[0])
 
     items = []
     for i,j in enumerate(encodings):
         items.append([j,i])
- 
+
     items = sorted(items,key=dist)
     print items[0][1]
     print items[1][1]
-    print items[len(items)-1][1]    
+    print items[len(items)-1][1]
