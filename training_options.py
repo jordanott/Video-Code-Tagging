a2b0bad9ddd078f7a4b1c2f3328dcadf65549e8d
Jordan Ott <jordanott365@gmail.com>
2018-02-09 14:36:41 -0800

    Added python java training ops

--- training_options.py
+++ training_options.py
@@ -9,6 +9,122 @@ sys.path.append('Models/CNN/')
 from model import Inception,VGG
 import os
 
+def java_python(x_train,y_train,x_test,y_test,px_train,py_train,px_test,py_test):
+    X_TRAIN,Y_TRAIN,X_TEST,Y_TEST = [],[],[],[]
+    # java train
+    for i in range(len(x_train)):
+        if np.all(y_train[i] == np.array([1,0,0,0])):
+            Y_TRAIN.append([1,0])
+            X_TRAIN.append(x_train[i])
+    # java test
+    for i in range(len(x_test)):
+        if np.all(y_test[i] == np.array([1,0,0,0])):
+            Y_TEST.append([1,0])
+            X_TEST.append(x_test[i])
+    # python train
+    for i in range(len(px_train)):
+        if np.all(py_train[i] == np.array([1,0,0,0])):
+            Y_TRAIN.append([0,1])
+            X_TRAIN.append(px_train[i])
+    # python test
+    for i in range(len(px_test)):
+        if np.all(py_test[i] == np.array([1,0,0,0])):
+            Y_TEST.append([0,1])
+            X_TEST.append(px_test[i])
+
+    model = VGG((300,300,3),2)
+    return np.array(X_TRAIN),np.array(Y_TRAIN),np.array(X_TEST),np.array(Y_TEST),model,'java_python.h5'
+
+def java_python_no_code(x_train,y_train,x_test,y_test,px_train,py_train,px_test,py_test):
+    X_TRAIN,Y_TRAIN,X_TEST,Y_TEST = [],[],[],[]
+    # java train
+    for i in range(len(x_train)):
+        if np.all(y_train[i] == np.array([1,0,0,0])):
+            Y_TRAIN.append([1,0,0])
+            X_TRAIN.append(x_train[i])
+        elif np.all(y_train[i] == np.array([0,0,0,1])):
+            Y_TRAIN.append([0,0,1])
+            X_TRAIN.append(x_train[i])
+    # java test
+    for i in range(len(x_test)):
+        if np.all(y_test[i] == np.array([1,0,0,0])):
+            Y_TEST.append([1,0,0])
+            X_TEST.append(x_test[i])
+        elif np.all(y_test[i] == np.array([0,0,0,1])):
+            Y_TEST.append([0,0,1])
+            X_TEST.append(x_test[i])
+
+    # python train
+    for i in range(len(px_train)):
+        if np.all(py_train[i] == np.array([1,0,0,0])):
+            Y_TRAIN.append([0,1,0])
+            X_TRAIN.append(px_train[i])
+        elif np.all(py_train[i] == np.array([0,0,0,1])):
+            Y_TRAIN.append([0,0,1])
+            X_TRAIN.append(px_train[i])
+    # python test
+    for i in range(len(px_test)):
+        if np.all(py_test[i] == np.array([1,0,0,0])):
+            Y_TEST.append([0,1,0])
+            X_TEST.append(px_test[i])
+        elif np.all(py_test[i] == np.array([0,0,0,1])):
+            Y_TEST.append([0,0,1])
+            X_TEST.append(px_test[i])
+
+    model = VGG((300,300,3),3)
+    return np.array(X_TRAIN),np.array(Y_TRAIN),np.array(X_TEST),np.array(Y_TEST),model,'java_python_no_code.h5'
+
+def java_python_pv_no_code(x_train,y_train,x_test,y_test,px_train,py_train,px_test,py_test):
+    X_TRAIN,Y_TRAIN,X_TEST,Y_TEST = [],[],[],[]
+    # java train
+    for i in range(len(x_train)):
+        if np.all(y_train[i] == np.array([1,0,0,0])):
+            Y_TRAIN.append([1,0,0])
+            X_TRAIN.append(x_train[i])
+        elif np.all(y_train[i] == np.array([0,1,0,0])):
+            Y_TRAIN.append([1,0,0])
+            X_TRAIN.append(x_train[i])
+        elif np.all(y_train[i] == np.array([0,0,0,1])):
+            Y_TRAIN.append([0,0,1])
+            X_TRAIN.append(x_train[i])
+    # java test
+    for i in range(len(x_test)):
+        if np.all(y_test[i] == np.array([1,0,0,0])):
+            Y_TEST.append([1,0,0])
+            X_TEST.append(x_test[i])
+        elif np.all(y_test[i] == np.array([0,1,0,0])):
+            Y_TEST.append([1,0,0])
+            X_TEST.append(x_test[i])
+        elif np.all(y_test[i] == np.array([0,0,0,1])):
+            Y_TEST.append([0,0,1])
+            X_TEST.append(x_test[i])
+
+    # python train
+    for i in range(len(px_train)):
+        if np.all(py_train[i] == np.array([1,0,0,0])):
+            Y_TRAIN.append([0,1,0])
+            X_TRAIN.append(px_train[i])
+        elif np.all(py_train[i] == np.array([0,1,0,0])):
+            Y_TRAIN.append([0,1,0])
+            X_TRAIN.append(px_train[i])
+        elif np.all(py_train[i] == np.array([0,0,0,1])):
+            Y_TRAIN.append([0,0,1])
+            X_TRAIN.append(px_train[i])
+    # python test
+    for i in range(len(px_test)):
+        if np.all(py_test[i] == np.array([1,0,0,0])):
+            Y_TEST.append([0,1,0])
+            X_TEST.append(px_test[i])
+        elif np.all(py_test[i] == np.array([0,1,0,0])):
+            Y_TEST.append([0,1,0])
+            X_TEST.append(px_test[i])
+        elif np.all(py_test[i] == np.array([0,0,0,1])):
+            Y_TEST.append([0,0,1])
+            X_TEST.append(px_test[i])
+
+    model = VGG((300,300,3),3)
+    return np.array(X_TRAIN),np.array(Y_TRAIN),np.array(X_TEST),np.array(Y_TEST),model,'java_python_pv_no_code.h5'
+
 def code_vs_no_code_strict(x_train,y_train,x_test,y_test):
     X_TRAIN,Y_TRAIN,X_TEST,Y_TEST = [],[],[],[]
     for i in range(len(x_train)):
